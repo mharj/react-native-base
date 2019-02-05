44173107adde980749a3d7513d14ba9471d782ba
Marko Harjula <marko.harjula@gmail.com>
2019-02-05 14:03:45 +0200

    version updates

--- i18n.js
+++ i18n.js
@@ -1,7 +1,7 @@
-import i18n from 'i18next';
+import i18next from 'i18next';
 import LanguageDetector from 'i18next-react-native-language-detector';
 
-i18n.use(LanguageDetector).init({
+i18next.use(LanguageDetector).init({
 	resources: {
 		en: {
 			translations: {
@@ -55,4 +55,4 @@ i18n.use(LanguageDetector).init({
 		wait: true,
 	},
 });
-export default i18n;
+export default i18next;
